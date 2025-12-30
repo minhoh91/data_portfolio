735507721668dab0ae61e7205ccb685d33f73abf
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-30 09:38:28 -0800

    Update regional_transactions.sql

--- regional_transactions.sql
+++ regional_transactions.sql
@@ -1,3 +1,13 @@
--- Provide a breakdown of total purchase volume and the number of transactions by sales region, ordered from highest to lowest spend.
+-- Provide a breakdown of total purchase volume and the number of purchases by sales region, ordered from highest to lowest spend.
 SELECT
-FROM
+  sales_region
+  , COUNT(transaction_id) as purchase_count
+  , SUM(amount) AS total_purchase_amount
+FROM users u
+JOIN transactions t
+  ON u.user_id = t.user_id
+WHERE transaction_type = 'purchase'
+GROUP BY sales_region
+ORDER BY SUM(amount) DESC
+;
+
