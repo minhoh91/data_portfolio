b46179beecfc5546a35acb738b34b5cbc3ef0091
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-29 16:46:59 -0800

    Create elite_users.sql

---
+++ elite_users.sql
@@ -0,0 +1,11 @@
+-- Identify the elite users who drive the most value. 
+-- Business Logic: Find all users who, in the last 30 days, 
+-- have made at least 3 direct deposits and at least 10 purchases.
+SELECT
+	user_id
+    , SUM(CASE WHEN transaction_type = 'direct_depot' THEN 1 ELSE 0 end) AS DD_count
+    , SUM(CASE WHEN transaction_type = 'purchase' THEN 1 ELSE 0 END) AS P_count
+FROM transactions
+WHERE transaction_date >= CURRENT_DATE() - INTERVAL 30 DAY
+GROUP BY user_id
+HAVING DD_count > 3 AND P_count >= 10;
