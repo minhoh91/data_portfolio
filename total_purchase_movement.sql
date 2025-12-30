37b8ddee4dee58965b36b4fb96c279f7a33796ec
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-30 09:54:50 -0800

    Update total_purchase_movement.sql

--- total_purchase_movement.sql
+++ total_purchase_movement.sql
@@ -1,1 +1,9 @@
 -- Generate a report showing the total purchase volume aggregated by month, ordered chronologically, to help us identify seasonal spending trends.
+
+SELECT
+  YEAR(transaction_date) AS yr
+  , MONTH(transaction_date) AS mth
+  , SUM(amount)
+FROM transactions
+WHERE transaction_type = 'purchase'
+GROUP BY 
