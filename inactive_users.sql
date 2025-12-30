829723b63fdb58b10debb67fad13ac51569fbfdb
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-30 08:03:33 -0800

    Create inactive_users.sql

---
+++ inactive_users.sql
@@ -0,0 +1,3 @@
+-- Identify users who were active in the past but have had zero transactions in the last 30 days. 
+-- Business Logic: We want to target these people with a "We miss you" email. Join the users table to the transactions table to find people with no recent activity.
+
