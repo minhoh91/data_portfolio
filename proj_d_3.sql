55dcfab13ccad327bb540ef147833d8f09ed85bc
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2026-01-05 11:56:00 -0800

    Create proj_d_3.sql

---
+++ proj_d_3.sql
@@ -0,0 +1,27 @@
+-- For each user, calculate the total number of distinct merchant categories they shop in. 
+-- Compare this against the platform-wide average to categorize users by "Engagement Persona."
+
+WITH user_category_count AS(
+SELECT
+  t.user_id
+  , COUNT(DISTINCT m.category) AS unique_category_count
+FROM transactions t
+JOIN merchants m ON t.merchant_id = m.merchant_id
+GROUP BY t.user_id
+),
+
+platform_average AS(
+SELECT
+  AVG(unique_category_count) AS avg_categories
+FROM user_category_count
+)
+
+SELECT
+  u.user_id
+  , u.unique_category_count
+  , CASE WHEN u.unique_category_count > avg_categories THEN 'High'
+  WHEN u.unique_category_count = p.avg_categories THEN 'Average'
+  ELSE 'Low' END AS engagement_persona
+FROM user_category_count u 
+CROSS JOIN platform_average p
+;
