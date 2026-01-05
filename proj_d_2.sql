1f653cf199144105506b71728ee3a77918283afe
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2026-01-05 11:21:33 -0800

    Update proj_d_2.sql

--- proj_d_2.sql
+++ proj_d_2.sql
@@ -2,4 +2,17 @@
 -- Calculate the average transaction amount and flag any merchant where the average exceeds $500.
 
 SELECT
-FROM
+  m.merchant_id
+  , m.merchant_name
+  , m.risk_level
+  , COUNT(transaction_id) AS transaction_count
+  , AVG(t.amount) AS transaction_average
+  , CASE WHEN AVG(transaction_amount) > 500 THEN 'HIGH RISK' ELSE 'MONITOR' END AS audit
+FROM transactions t
+JOIN merchants m ON t.merchant_id = m.merchant_id
+WHERE risk_level >= 4
+  AND transaction_type = 'Purchase'
+  AND tranction_date >= CURDATE() - INTERVAL 60 DAY
+GROUP BY merchant_id, merchant_name, risk_level
+HAVING COUNT(transction_id) > 50
+;
