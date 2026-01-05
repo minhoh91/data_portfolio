ca15a75e4a40e61ea03c67a15cea5fd38ff28c86
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2026-01-05 10:37:44 -0800

    Create proj_d_2.sql

---
+++ proj_d_2.sql
@@ -0,0 +1,5 @@
+-- Identify merchants with a risk_level of 4 or 5 that have processed more than 50 transactions in the last 60 days. 
+-- Calculate the average transaction amount and flag any merchant where the average exceeds $500.
+
+SELECT
+FROM
